fd0304828911cca3abafe1e967f9762be26dd702
stylus1274 <6.390707e+06+stylus1274@users.noreply.github.com>
2026-09-11 12:08:41 +0000

    Prevent mobile chat control overlap

--- components/SiteContent.tsx
+++ components/SiteContent.tsx
@@ -71,7 +71,6 @@ function replyFor(message: string) {
 
 export function SiteContent({ title, html, cta }: Props) {
   const rootRef = useRef<HTMLDivElement>(null);
-  const isMobileChatSuppressedConversionPage = /data-tf-live="(?:01JG1WQE1DWSDV5R9XDWD3PJRW|01KREH6BX2SN5A0Y490YPMRKR8)"/.test(html);
 
   useEffect(() => {
     const root = rootRef.current;
@@ -306,5 +305,5 @@ export function SiteContent({ title, html, cta }: Props) {
     };
   }, [html, title, cta]);
 
-  return <div data-site-content data-mobile-chat-suppressed={isMobileChatSuppressedConversionPage ? "true" : undefined} ref={rootRef} dangerouslySetInnerHTML={{ __html: html }} />;
+  return <div data-site-content ref={rootRef} dangerouslySetInnerHTML={{ __html: html }} />;
 }
